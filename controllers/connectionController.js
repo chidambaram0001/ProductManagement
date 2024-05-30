a65a8f06fe51f1b49bcbb4b26844ab3ec56ace20
chidambaram0001 <96237777+chidambaram0001@users.noreply.github.com>
2024-05-30 10:21:18 +0530

    Create connectionController.js

---
+++ controllers/connectionController.js
@@ -0,0 +1,34 @@
+'use strict';
+const express = require("express");
+const mongoose = require("mongoose");
+
+//module.exports.connectionData = null;
+
+
+
+ module.exports.connectMongoDB =  async function(){
+    try{
+         mongoose.connect("mongodb://localhost:27017/products",{
+            useNewUrlParser: true // Boilerplate for Mongoose 5.x
+          });
+        mongoose.connection.on('connecting', () => { 
+            console.log('connecting')
+            console.log(mongoose.connection.readyState); //logs 2
+          });
+          mongoose.connection.on('connected', () => {
+            console.log('connected');
+            console.log(mongoose.connection.readyState); //logs 1
+          });
+          mongoose.connection.on('disconnecting', () => {
+            console.log('disconnecting');
+            console.log(mongoose.connection.readyState); // logs 3
+          });
+          mongoose.connection.on('disconnected', () => {
+            console.log('disconnected');
+            console.log(mongoose.connection.readyState); //logs 0
+          });
+  
+  }catch(err){
+  console.log(err);
+  }
+ }
